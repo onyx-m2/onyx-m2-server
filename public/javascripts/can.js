2bcc631e24129db98e712df286adc55a5beb1012
John McCalla <johnmccalla@users.noreply.github.com>
2020-04-24 14:19:30 -0400

    feat: add a sync button to can panel for pausing

--- public/javascripts/can.js
+++ public/javascripts/can.js
@@ -1,20 +1,23 @@
 $(() => {
 
   const m2 = new M2()
+  var enabled = true
 
   m2.addEventListener('connect', () => {
     $('#disconnected').addClass('hidden')
   })
 
   m2.addEventListener('message', (event) => {
-    const { message } = event
-    const { id, mnemonic, ts, value } = message
-    const output = `${id} ${mnemonic} @ ${ts} => ${value}`
-    const $msg = $(`#msg${id}`)
-    if ($msg.length == 0) {
-      $(`<div id="msg${id}">${output}</div>'`).appendTo('#log')
-    } else {
-      $msg.text(output)
+    if (enabled) {
+      const { message } = event
+      const { id, mnemonic, ts, value } = message
+      const output = `${id} ${mnemonic} @ ${ts} => ${value}`
+      const $msg = $(`#msg${id}`)
+      if ($msg.length == 0) {
+        $(`<div id="msg${id}">${output}</div>'`).appendTo('#log')
+      } else {
+        $msg.text(output)
+      }
     }
   })
 
@@ -23,4 +26,12 @@ $(() => {
     $('#reason').text(`(${event.reason})`)
   })
 
+  $('#toggle').click(() => {
+    if (enabled) {
+      $('#toggle').addClass('basic')
+    } else {
+      $('#toggle').removeClass('basic')
+    }
+    enabled = !enabled
+  })
 })
