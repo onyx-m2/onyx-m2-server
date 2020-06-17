80056477e4c74c2704ddabcb7c6db2d0655559a2
John McCalla <johnmccalla@users.noreply.github.com>
2020-06-17 12:16:38 -0400

    chore: add debug string for db

--- m2.js
+++ m2.js
@@ -285,11 +285,13 @@ async function processMessage(ws, msg) {
   // if it's the first one, insert a first message, returning its id to be able
   // to create a new segment
   else {
-    const { tid: msgId } = await pg.one(sql`
+    const d = await pg.one(sql`
       INSERT INTO canbus_msgs (ts, id, data)
       VALUES (${ts}, ${id}, ${value.buffer})
       RETURNING tid
     `)
+    log.debug(d)
+    const { tid: msgId } = d
 
     const { tid: segmentId } = await pg.one(sql`
       INSERT INTO canbus_segments (start_id)
