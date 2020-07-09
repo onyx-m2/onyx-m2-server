b0e51394179b139b8fcc1a424db32e6acd5565ed
John McCalla <johnmccalla@users.noreply.github.com>
2020-07-09 10:57:40 -0400

    fix: proxy cms calls before doing any parsing

--- app.js
+++ app.js
@@ -7,20 +7,21 @@ const log = require('./logger')
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
 const app = express()
+
+// proxy the cms so that both servers can be on the same host
+if (process.env.CMS_PORT) {
+  app.use('/cms', createProxyMiddleware({
+    target: `http://localhost:${process.env.CMS_PORT}`,
+    pathRewrite: {'^/cms' : ''}
+  }))
+}
+
 app.set('trust proxy', 1)
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 
-// proxy the cms so that both servers can be on the same host
-if (process.env.CMS_PORT) {
-  app.use('/cms', createProxyMiddleware({
-    target: `http://localhost:${process.env.CMS_PORT}`,
-    changeOrigin: true,
-    pathRewrite: {'^/cms' : ''}
-  }))
-}
 
 // log incoming requests
 app.use((req, res, next) => {
